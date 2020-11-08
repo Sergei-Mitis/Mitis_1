600d912924fae74465358535e086189852d29b50
Sergei-Mitis <mit1ss@mail.ru>
2020-11-08 12:44:36 +0500

    php

---
+++ logotip.inc.php
@@ -0,0 +1,5 @@
+    
+                <div class="logotip"> 
+                <img src="img/1.png" width="600" height="800" alt="photo">
+                </div>                                                 
+        
